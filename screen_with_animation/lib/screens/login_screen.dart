d6147daebb0ebc86dca5d87592a39ebcb20d21d0
Freyr <freyrecanbryan@gmail.com>
2025-09-04 10:41:41 -0600

    feat(login): add email and password inputs

--- screen_with_animation/lib/screens/login_screen.dart
+++ screen_with_animation/lib/screens/login_screen.dart
@@ -9,16 +9,81 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  // Declaramos las variables necesarias
+  late TextEditingController _userPasswordController;
+  bool _passwordVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _userPasswordController = TextEditingController();
+    _passwordVisible = false;
+  }
+
+  @override
+  void dispose() {
+    _userPasswordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: RiveAnimation.asset("assets/animated_login_character.riv"),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            children: [
+              SizedBox(
+                width: size.width,
+                height: 200,
+                child: const RiveAnimation.asset(
+                  'assets/animated_login_character.riv',
+                ),
+              ),
+              const SizedBox(height: 10),
+              // Campo de texto email
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  hintText: 'Email',
+                  labelText: 'Email',
+                  prefixIcon: const Icon(Icons.mail),
+                ),
+              ),
+              const SizedBox(height: 10),
+              // Campo de texto password
+              TextField(
+                controller: _userPasswordController,
+                obscureText: !_passwordVisible,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  hintText: 'Password',
+                  labelText: 'Password',
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _passwordVisible
+                          ? Icons.visibility_off
+                          : Icons.visibility,
+                      color: Colors.grey,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _passwordVisible = !_passwordVisible;
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
