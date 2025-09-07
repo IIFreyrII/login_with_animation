a61b0e3d37e27fe34058c973ed098beda57f6482
Freyr <freyrecanbryan@gmail.com>
2025-09-07 15:13:34 -0600

    Linux

--- screen_with_animation/README.md
+++
@@ -1,3 +0,0 @@
-# screen_with_animation
-
-A new Flutter project.
--- screen_with_animation/android/app/src/main/kotlin/com/example/screen_with_animation/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.screen_with_animation
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
